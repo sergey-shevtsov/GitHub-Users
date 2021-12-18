d5e508558a07b8f3899b182d4187fa56abf0b077
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-18 20:53:16 +0300

    Добавил интерфейс GitHubApi с методами для обращения к api и GitHubApiFactory для получения экземпляра GitHubApi.

---
+++ app/src/main/java/com/sshevtsov/githubusers/data/retrofit/GitHubApi.kt
@@ -0,0 +1,20 @@
+package com.sshevtsov.githubusers.data.retrofit
+
+import com.sshevtsov.githubusers.data.GitHubRepoDto
+import com.sshevtsov.githubusers.data.GitHubUserDto
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubApi {
+
+    @GET("/users")
+    fun getUsers(): Single<List<GitHubUserDto>>
+
+    @GET("/users/{login}")
+    fun getUserByLogin(@Path("login") login: String): Single<GitHubUserDto>
+
+    @GET("/users/{login}/repos")
+    fun getUserRepositories(@Path("login") login: String): Single<List<GitHubRepoDto>>
+
+}
