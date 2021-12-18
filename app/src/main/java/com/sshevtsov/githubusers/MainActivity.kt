fbe3da5394654274aaf2759ce91f306043e2de70
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-18 19:45:20 +0300

    Добавил открытие фрагмента с пользователями при старте приложения.

--- app/src/main/java/com/sshevtsov/githubusers/MainActivity.kt
+++ app/src/main/java/com/sshevtsov/githubusers/MainActivity.kt
@@ -2,15 +2,31 @@ package com.sshevtsov.githubusers
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.sshevtsov.githubusers.databinding.ActivityMainBinding
+import com.sshevtsov.githubusers.mvpusers.UsersScreen
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    private val navigator = AppNavigator(this, R.id.fragment_container)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) app.router.navigateTo(UsersScreen)
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        app.navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        app.navigatorHolder.removeNavigator()
+        super.onPause()
     }
 }
