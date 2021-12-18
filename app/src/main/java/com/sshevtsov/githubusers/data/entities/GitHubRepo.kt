9763bef245249f719686de8987b62755e4259aba
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-18 21:30:25 +0300

    Добавил сущности для пользователя и репозитория в отдельный пакет entities.

---
+++ app/src/main/java/com/sshevtsov/githubusers/data/entities/GitHubRepo.kt
@@ -0,0 +1,9 @@
+package com.sshevtsov.githubusers.data.entities
+
+data class GitHubRepo(
+    val id: String? = null,
+    val name: String? = null,
+    val htmlUrl: String? = null,
+    val language: String? = null,
+    val stargazersCount: String? = null,
+)
---
+++ app/src/main/java/com/sshevtsov/githubusers/data/entities/GitHubUser.kt
@@ -0,0 +1,9 @@
+package com.sshevtsov.githubusers.data.entities
+
+data class GitHubUser(
+    val id: String? = null,
+    val login: String? = null,
+    val avatarUrl: String? = null,
+    val htmlUrl: String? = null,
+    val reposUrl: String? = null
+)
