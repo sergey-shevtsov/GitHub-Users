b2db33c6eb67c7916900d5af20533af9c3f1a10b
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-18 19:42:21 +0300

    Добавил расширения для доступа к контексту приложения, добавил router и navigationHolder для Cicerone.

--- app/src/main/java/com/sshevtsov/githubusers/App.kt
+++ app/src/main/java/com/sshevtsov/githubusers/App.kt
@@ -1,6 +1,20 @@
 package com.sshevtsov.githubusers
 
 import android.app.Application
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.github.terrakok.cicerone.Cicerone
 
 class App : Application() {
+
+    private val cicerone = Cicerone.create()
+    val router get() = cicerone.router
+    val navigatorHolder get() = cicerone.getNavigatorHolder()
+
 }
+
+val Context.app: App
+    get() = applicationContext as App
+
+val Fragment.app: App
+    get() = requireContext().app
