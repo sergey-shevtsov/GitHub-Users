9e39463333d65dd1ea0bc3411998f3bd8502a5cd
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-19 19:12:47 +0300

    Добавил необходимые методы в UserView.

--- app/src/main/java/com/sshevtsov/githubusers/mvpuser/UserView.kt
+++ app/src/main/java/com/sshevtsov/githubusers/mvpuser/UserView.kt
@@ -1,6 +1,23 @@
 package com.sshevtsov.githubusers.mvpuser
 
+import com.sshevtsov.githubusers.ViewState
+import com.sshevtsov.githubusers.data.entities.GitHubRepoEntity
+import com.sshevtsov.githubusers.data.entities.GitHubUserEntity
 import moxy.MvpView
+import moxy.viewstate.strategy.alias.SingleState
+import moxy.viewstate.strategy.alias.Skip
 
 interface UserView : MvpView {
+
+    @Skip
+    fun setMainState(viewState: ViewState)
+
+    @Skip
+    fun setRepoListState(viewState: ViewState)
+
+    @SingleState
+    fun showUserContent(userEntity: GitHubUserEntity)
+
+    @SingleState
+    fun showRepositories(repositories: List<GitHubRepoEntity>)
 }
