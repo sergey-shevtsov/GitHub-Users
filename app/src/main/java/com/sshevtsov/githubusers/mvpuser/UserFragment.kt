1fcf9d3b2e21c7bee7fdef3958969d58405d9d35
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-19 19:13:32 +0300

    Настроил загрузку данных и отображение статуса загрузки.

--- app/src/main/java/com/sshevtsov/githubusers/mvpuser/UserFragment.kt
+++ app/src/main/java/com/sshevtsov/githubusers/mvpuser/UserFragment.kt
@@ -1,10 +1,24 @@
 package com.sshevtsov.githubusers.mvpuser
 
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.sshevtsov.githubusers.R
+import com.sshevtsov.githubusers.ViewState
+import com.sshevtsov.githubusers.data.entities.GitHubRepoEntity
+import com.sshevtsov.githubusers.data.entities.GitHubUserEntity
 import com.sshevtsov.githubusers.data.remote.GitHubUserRepositoryFactory
 import com.sshevtsov.githubusers.databinding.FragmentUserBinding
+import com.sshevtsov.githubusers.mvpuser.recycler.RepositoriesAdapter
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
 
@@ -13,8 +27,10 @@ class UserFragment : MvpAppCompatFragment(R.layout.fragment_user), UserView {
     private var _binding: FragmentUserBinding? = null
     private val binding get() = _binding!!
 
+    private val repositoriesAdapter = RepositoriesAdapter()
+
     private val presenter by moxyPresenter {
-        UserPresenter(GitHubUserRepositoryFactory.create())
+        UserPresenter(GitHubUserRepositoryFactory.create(), userLogin)
     }
 
     private val userLogin by lazy {
@@ -24,6 +40,58 @@ class UserFragment : MvpAppCompatFragment(R.layout.fragment_user), UserView {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentUserBinding.bind(view)
+
+        binding.repositoriesRecycler.adapter = repositoriesAdapter
+    }
+
+    override fun setMainState(viewState: ViewState) {
+        when (viewState) {
+            ViewState.LOADING -> binding.mainProgressIndicatorFrameLayout.isVisible = true
+            ViewState.IDLE -> binding.mainProgressIndicatorFrameLayout.isVisible = false
+        }
+    }
+
+    override fun setRepoListState(viewState: ViewState) {
+        when (viewState) {
+            ViewState.LOADING -> binding.repositoriesProgressIndicator.isVisible = true
+            ViewState.IDLE -> binding.repositoriesProgressIndicator.isVisible = false
+        }
+    }
+
+    override fun showUserContent(userEntity: GitHubUserEntity) {
+        binding.loginTextView.text = userEntity.login
+        binding.avatarProgressIndicator.isVisible = true
+
+        Glide.with(requireContext())
+            .load(userEntity.avatarUrl)
+            .error(R.drawable.ic_image_error)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.avatarProgressIndicator.isVisible = false
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.avatarProgressIndicator.isVisible = false
+                    return false
+                }
+            })
+            .into(binding.avatarImageView)
+    }
+
+    override fun showRepositories(repositories: List<GitHubRepoEntity>) {
+        repositoriesAdapter.submitList(repositories)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/sshevtsov/githubusers/mvpuser/UserPresenter.kt
+++ app/src/main/java/com/sshevtsov/githubusers/mvpuser/UserPresenter.kt
@@ -1,9 +1,44 @@
 package com.sshevtsov.githubusers.mvpuser
 
+import com.sshevtsov.githubusers.ViewState
 import com.sshevtsov.githubusers.data.remote.GitHubUserRepository
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import moxy.MvpPresenter
+import java.util.concurrent.TimeUnit
 
 class UserPresenter(
-    private val userRepository: GitHubUserRepository
+    private val userRepository: GitHubUserRepository,
+    private val userLogin: String
 ) : MvpPresenter<UserView>() {
+
+    override fun onFirstViewAttach() {
+        updateUserContent(userLogin)
+        updateRepositoriesContent(userLogin)
+    }
+
+    private fun updateUserContent(userLogin: String) {
+        viewState.setMainState(ViewState.LOADING)
+
+        userRepository.getUserByLogin(userLogin)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doFinally { viewState.setMainState(ViewState.IDLE) }
+            .subscribe({
+                viewState.showUserContent(it)
+            },{
+                //todo
+            })
+    }
+
+    private fun updateRepositoriesContent(userLogin: String) {
+        viewState.setRepoListState(ViewState.LOADING)
+
+        userRepository.getUserRepositories(userLogin)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doFinally { viewState.setRepoListState(ViewState.IDLE) }
+            .subscribe({
+                viewState.showRepositories(it)
+            },{
+                //todo
+            })
+    }
 }
