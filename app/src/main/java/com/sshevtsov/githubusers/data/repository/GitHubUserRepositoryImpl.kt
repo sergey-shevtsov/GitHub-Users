a862abd2ed1b2120a0ab01910aa5a9b050b8afdc
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-23 22:46:05 +0300

    Добавил кэширование пользователей и репозиториев.

--- app/src/main/java/com/sshevtsov/githubusers/data/repository/GitHubUserRepositoryImpl.kt
+++ app/src/main/java/com/sshevtsov/githubusers/data/repository/GitHubUserRepositoryImpl.kt
@@ -2,10 +2,11 @@ package com.sshevtsov.githubusers.data.repository
 
 import com.sshevtsov.githubusers.data.entities.GitHubRepositoryEntity
 import com.sshevtsov.githubusers.data.entities.GitHubUserEntity
-import com.sshevtsov.githubusers.data.retrofit.GitHubDtoMapper
 import com.sshevtsov.githubusers.data.retrofit.GitHubApi
+import com.sshevtsov.githubusers.data.retrofit.GitHubDtoMapper
 import com.sshevtsov.githubusers.data.room.GitHubRepositoryDao
 import com.sshevtsov.githubusers.data.room.GitHubUserDao
+import com.sshevtsov.githubusers.data.room.RoomGitHubMapper
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
@@ -16,20 +17,51 @@ class GitHubUserRepositoryImpl(
 ) : GitHubUserRepository {
 
     override fun getUsers(): Single<List<GitHubUserEntity>> {
-        return gitHubApi.getUsers()
-            .map { GitHubDtoMapper.mapUserDtoListToEntityList(it) }
+        return roomUserDao.getAll()
+            .flatMap {
+                if (it.isEmpty()) {
+                    gitHubApi.getUsers()
+                        .map { resultFromServer ->
+                            val users = GitHubDtoMapper.mapUserDtoToEntity(resultFromServer)
+                            roomUserDao.insert(RoomGitHubMapper.mapUserEntityToRoomUser(users))
+                            users
+                        }
+                } else {
+                    Single.just(RoomGitHubMapper.mapRoomUserToUserEntity(it))
+                }
+            }
             .subscribeOn(Schedulers.io())
     }
 
     override fun getUserByLogin(login: String): Single<GitHubUserEntity> {
-        return gitHubApi.getUserByLogin(login)
-            .map { GitHubDtoMapper.mapUserDtoToEntity(it) }
+        return roomUserDao.findByLogin(login)
+            .flatMap { Single.just(RoomGitHubMapper.mapRoomUserToUserEntity(it)) }
             .subscribeOn(Schedulers.io())
     }
 
     override fun getUserRepositories(login: String): Single<List<GitHubRepositoryEntity>> {
-        return gitHubApi.getUserRepositories(login)
-            .map { GitHubDtoMapper.mapRepoDtoListToEntityList(it) }
+        return roomUserDao.findByLogin(login)
+            .flatMap { user ->
+                roomRepositoryDao.findForUser(user.id)
+                    .flatMap {
+                        if (it.isEmpty()) {
+                            gitHubApi.getUserRepositories(login)
+                                .map { resultFromServer ->
+                                    val repositories =
+                                        GitHubDtoMapper.mapRepoDtoToEntity(resultFromServer)
+                                    roomRepositoryDao.insert(
+                                        RoomGitHubMapper.mapRepositoryEntityToRoomRepository(
+                                            repositories,
+                                            user.id
+                                        )
+                                    )
+                                    repositories
+                                }
+                        } else {
+                            Single.just(RoomGitHubMapper.mapRoomRepositoryToRepositoryEntity(it))
+                        }
+                    }
+            }
             .subscribeOn(Schedulers.io())
     }
 }
