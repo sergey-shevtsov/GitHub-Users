6a28b6a71cd6665df214cc7147a81404f29a5af1
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-23 18:34:37 +0300

    Создал dao для репозитория.

---
+++ app/src/main/java/com/sshevtsov/githubusers/data/room/GitHubRepositoryDao.kt
@@ -0,0 +1,42 @@
+package com.sshevtsov.githubusers.data.room
+
+import androidx.room.*
+import androidx.room.OnConflictStrategy.REPLACE
+
+@Dao
+interface GitHubRepositoryDao {
+
+    @Insert(onConflict = REPLACE)
+    fun insert(repository: RoomGitHubRepository)
+
+    @Insert(onConflict = REPLACE)
+    fun insert(vararg repositories: RoomGitHubRepository)
+
+    @Insert(onConflict = REPLACE)
+    fun insert(repositories: List<RoomGitHubRepository>)
+
+    @Update
+    fun update(repository: RoomGitHubRepository)
+
+    @Update
+    fun update(vararg repositories: RoomGitHubRepository)
+
+    @Update
+    fun update(repositories: List<RoomGitHubRepository>)
+
+    @Delete
+    fun delete(repository: RoomGitHubRepository)
+
+    @Delete
+    fun delete(vararg repositories: RoomGitHubRepository)
+
+    @Delete
+    fun delete(repositories: List<RoomGitHubRepository>)
+
+    @Query("SELECT * FROM GitHubRepositoryTable")
+    fun getAll(): List<RoomGitHubRepository>
+
+    @Query("SELECT * FROM GitHubRepositoryTable WHERE userId = :userId")
+    fun findForUser(userId: String): List<RoomGitHubRepository>
+
+}
