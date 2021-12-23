e71fbb3cc14bfa59346d842178202e58ea18da18
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-12-23 18:44:54 +0300

    Добавил создание базы данных в классе App.

--- app/src/main/java/com/sshevtsov/githubusers/App.kt
+++ app/src/main/java/com/sshevtsov/githubusers/App.kt
@@ -3,13 +3,24 @@ package com.sshevtsov.githubusers
 import android.app.Application
 import android.content.Context
 import androidx.fragment.app.Fragment
+import androidx.room.Room
 import com.github.terrakok.cicerone.Cicerone
+import com.sshevtsov.githubusers.data.room.DBStorage
 
 class App : Application() {
 
     private val cicerone = Cicerone.create()
     val router get() = cicerone.router
     val navigatorHolder get() = cicerone.getNavigatorHolder()
+
+    private lateinit var database: DBStorage
+
+    override fun onCreate() {
+        super.onCreate()
+
+        database =
+            Room.databaseBuilder(this, DBStorage::class.java, "github.db").build()
+    }
 
 }
 
